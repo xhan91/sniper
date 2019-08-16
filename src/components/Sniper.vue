0a8cafed9ae73733cac26d24aaf6fad4e5965868
Josh Han <xhana@amazon.com>
2019-08-16 14:05:39 -0700

    functionalities done

--- src/components/Sniper.vue
+++ src/components/Sniper.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <textarea v-model="template" name="" id="" cols="30" rows="10" ></textarea>
-    <ul>
-      <li v-for="variable in varList" :key="variable">
-        {{variable}}
-      </li>
-    </ul>
+    <div>
+      <textarea v-model="template" cols="300" rows="10"></textarea>
+    </div>
+    <div v-for="(fillValuesEntry, k) in fillValues" :key="k">
+      <span v-for="variable in varList" :key="variable" style="padding-right: 20px">
+        <label for="variable" style="padding-right:5px">{{variable}}</label>
+        <input type="text" :id="variable" v-model="fillValuesEntry[variable]">
+      </span>
+      <button v-if="fillValues && fillValues.length > 1" v-on:click="removeFillValueEntry(k)">-</button>
+      <button v-on:click="addFillValueEntry(k)" >+</button>
+    </div>
+    <div style="margin-top: 20px">
+      <textarea readonly v-model="generatedCommand" cols="300" rows="50"></textarea>
+    </div>
   </div>
 </template>
 
@@ -15,24 +23,54 @@ export default {
   data() {
     return {
       template: '',
-      varList: []
+      varList: [],
+      fillValues: [], // [{key1: value1...}]
+      generatedCommand: 'aaa'
     }
   },
   watch: {
     template() {
-      this.extractVariables();
+      this.reset();
+    },
+    fillValues: {
+      deep: true,
+      handler() {
+        this.generateCommand();
+      }
     }
   },
   methods: {
+    reset() {
+      this.extractVariables();
+      this.$set(this.fillValues, 0, {})
+    },
+    addFillValueEntry(k) {
+      this.fillValues.splice(k+1,0,{});
+    },
+    removeFillValueEntry(k) {
+      this.fillValues.splice(k,1);
+    },
     extractVariables() {
       const varSet = new Set();
       const reg = /<(.+?)>/g
       const vars = this.template.matchAll(reg);
       for (const variable of vars) {
-        console.log(variable);
         varSet.add(variable[1]);
       }
       this.varList = Array.from(varSet);
+    },
+    generateCommand() {
+      console.log('I run');
+      let result = '';
+      for (const fillValuesEntry of this.fillValues) {
+        let t = this.template;
+        for (const key of Object.keys(fillValuesEntry)) {
+          const reg = new RegExp(`<${key}>`, 'g');
+          t = t.replace(reg, `${fillValuesEntry[key]}`);
+        }
+        result += `${t}\n`;
+      }
+      this.generatedCommand = result;
     }
   }
 }
